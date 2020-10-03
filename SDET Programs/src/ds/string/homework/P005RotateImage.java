ec4e96384fb381df862551c242b5261b4e0d0c09
Aravindhan Boopathi <aravindhanb@hotmail.com>
2020-10-03 21:32:57 +0530

    10/03

--- SDET Programs/src/ds/string/homework/P005RotateImage.java
+++ SDET Programs/src/ds/string/homework/P005RotateImage.java
@@ -11,6 +11,9 @@ public class P005RotateImage {
  input: {[1,2,3],
   		 [4,5,6],
   		 [7,8,9]}
+
+  		  
+  		  
  output: 
  		{[7,4,1],
  		 [8,5,2],
--- SDET Programs/src/ds/string/homework/P008ReverseWave.java
+++ SDET Programs/src/ds/string/homework/P008ReverseWave.java
@@ -13,6 +13,10 @@ Input :  1  2  3  4
          5  6  7  8
          9  10 11 12
          13 14 15 16
+         
+         {1,2,3,4},
+         {5,6,7,8},
+         {9,10,11,12}
 
 Output : 4 8 12 16 15 11 7 3 2 6 10 14 13 9 5 1
 
@@ -39,17 +43,17 @@ Pseudo-code:
 
 //		int[][] output = new int[input.length][input.length];
 
+		int row=input[0].length-1, column=input.length-1;
 
-
-		for(int i=input.length-1; i>=0; i--){
+		for(int i=Math.min(row, column); i>=0; i--){
 
 			if(i%2!=0)
-				for(int j=0; j<input.length; j++){
-					System.out.println(input[j][i]);
+				for(int j=0; j<Math.max(row, column); j++){
+					System.out.print(input[j][i] + " ");
 				}
 			else
-				for(int j=input.length-1; j>=0; j--){
-					System.out.println(input[j][i]);
+				for(int j=Math.max(row, column); j>=0; j--){
+					System.out.print(input[j][i] + " ");
 				}
 			
 		}
@@ -60,13 +64,28 @@ Pseudo-code:
 		}*/
 	}
 
-	@Test
-	public void testData1(){
+//	@Test
+//	public void testData1(){
+//
+//		int[][] input = {{1,2,3,4},
+//						 {5,6,7,8},
+//						 {9,10,11,12},
+//						 {13,14,15,16}};
+//
+//		for(int row[]: input){
+//
+//			System.out.println(Arrays.toString(row));
+//		}
+//
+//		solution(input);
+//		System.out.println();
+//
+//	}
 
-		int[][] input = {{1,2,3,4},
-						 {5,6,7,8},
-						 {9,10,11,12},
-						 {13,14,15,16}};
+	@Test
+	public void testData2(){
+
+		int[][] input = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
 
 		for(int row[]: input){
 
@@ -74,10 +93,10 @@ Pseudo-code:
 		}
 
 		solution(input);
-		System.out.println(input.length);
+//		System.out.println(input.length);
 
 	}
-
-
+	
+//	{{1,2,3,4},{5,6,7,8},{9,10,11,12}}
 
 }
